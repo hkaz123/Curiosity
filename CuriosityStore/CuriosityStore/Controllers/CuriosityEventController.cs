c5cb6aadd9a6cb5fe979c42fed55581c5f2d6667
hkaz123 <h.kazmi@hotmail.com>
2015-05-23 12:02:45 +0100

    Initial Commit

---
+++ CuriosityStore/CuriosityStore/Controllers/CuriosityEventController.cs
@@ -0,0 +1,34 @@
+﻿using CuriosityStore.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CuriosityStore.Controllers
+{
+    public class CuriosityEventController : ApiController
+    {
+        List<CuriosityEvent> _dummyEvents = new List<CuriosityEvent>()
+        {
+            new CuriosityEvent() { Name = "DanIsCool" },
+            new CuriosityEvent() { Name = "HasanIsCooler" }
+        };
+
+        public IEnumerable<CuriosityEvent> GetAll()
+        {
+            return _dummyEvents;
+        }
+
+        public IEnumerable<CuriosityEvent> GetRecent(TimeSpan periodBack)
+        {
+            return _dummyEvents;
+        }
+
+        public void Add(CuriosityEvent curiosityEvent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
