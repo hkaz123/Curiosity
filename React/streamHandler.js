50a2a54c239aab596b84bbddf6147a2a28f9ec5a
hkaz123 <h.kazmi@hotmail.com>
2015-06-01 16:57:19 +0100

    Initial commit for React UI modelling - stream handler for curiosities and packages.json

---
+++ React/streamHandler.js
@@ -0,0 +1,29 @@
+﻿var Curiosity = require('');
+
+module.exports = function(stream, io){
+
+  // When curiosities get sent our way ...
+  stream.on('data', function(data) {
+
+    // Construct a new curiosity object
+    var curiosity = {
+      Name: data['text'],
+      DateTime: data[''],
+      Description: data['text'],
+      
+    };
+
+    // Create a new model instance with our object
+    var curiosityEntry = new Curiosity(curiosity);
+
+    // Save 'er to the database
+    curiosityEntry.save(function(err) {
+      if (!err) {
+        // If everything is cool, socket.io emits the curiosity.
+        io.emit('curiosity', curiosity);
+      }
+    });
+
+  });
+
+};
